d7b4a15508be73c5ca5507f645d8f86bf308c58f
juhyeon jang <cjdool@naver.com>
2020-11-20 21:57:42 +0900

    privacy issue

--- mytest/settings.py
+++ mytest/settings.py
@@ -136,7 +136,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'juhyeonjang64@gmail.com'
-EMAIL_HOST_PASSWORD = 'tcr120607!'
+EMAIL_HOST_PASSWORD = ''
 SERVER_EMAIL = 'juhyeonjang64@gmail.com'
 DEFAULT_FROM_EMAIL = 'juhyeonjang64'
 
